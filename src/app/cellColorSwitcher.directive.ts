fdd34e983945afea15f66f242d5e44643024c0eb
dongyul an <dongyul.an.123@gmail.com>
2021-01-06 15:41:48 -0500

    one more

--- src/app/cellColorSwitcher.directive.ts
+++ src/app/cellColorSwitcher.directive.ts
@@ -1,4 +1,6 @@
-import { ContentChild, Directive, Input, SimpleChange } from "@angular/core";
+import {  ContentChildren, Directive, Input, SimpleChange,
+        QueryList } from "@angular/core";
+
 import { PaCellColor } from "./cellColor.directive";
 
 @Directive({
@@ -9,12 +11,18 @@ export class PaCellColorSwitcher{
     @Input("paCellDarkColor")
     modelProperty: Boolean;
 
-    @ContentChild(PaCellColor)
-    contentChild: PaCellColor;
+    @ContentChildren(PaCellColor)
+    contentChildren: QueryList<PaCellColor>;
 
     ngOnChanges( changes: { [property:string]: SimpleChange}){
-        if( this.contentChild != null){
-            this.contentChild.setColor( changes["modelProperty"].currentValue);
+            this.updateContentChildren( changes["modelProperty"].currentValue);
+    }
+
+    private updateContentChildren( dark:boolean){
+        if( this.contentChildren != null && dark != undefined){
+            this.contentChildren.forEach((child,index)=>{
+                    child.setColor(index%2? dark: !dark);
+            });
         }
     }
 }
